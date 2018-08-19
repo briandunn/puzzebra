cc698b6ac01500fe263a2531abd8c1450bcff8fc
Brian Dunn <brian@hashrocket.com>
2018-08-18 23:14:44 -0500

    Fetch a puzzle

--- src/puzzebra/core.cljs
+++ src/puzzebra/core.cljs
@@ -1,12 +1,68 @@
 (ns puzzebra.core
   (:require
     ["react-native" :as ReactNative]
+    [cognitect.transit :as t]
     [reagent.core :as r :refer [atom adapt-react-class reactify-component]]))
 
 (def text (adapt-react-class (.-Text ReactNative)))
 (def view (adapt-react-class (.-View ReactNative)))
 
+(def difficulties
+  {"beginner"
+   {:extra-clues 2
+    :clue-weights
+    {:in-house 1.2
+     :left-of 1.0
+     :same-house 1.3}
+    :ensured-clues
+    {:in-house 1
+     :same-house 2
+     :left-of 1}}
+   "normal"
+   {:extra-clues 1
+    :clue-weights
+    {:in-house 1.0
+     :left-of 1.0
+     :next-to 1.0
+     :same-house 1.0}
+    :ensured-clues
+    {:in-house 1
+     :same-house 1
+     :left-of 1}}
+   "expert"
+   {:extra-clues 0
+    :clue-weights
+    {:in-house 0.9
+     :left-of 1.1
+     :next-to 1.2
+     :same-house 1.1}
+    :ensured-clues
+    {:same-house 1
+     :left-of 1}}})
+
+(defn fetch-puzzle [config]
+  (let [body (t/write (t/writer :json) config)]
+    (->
+      (.fetch
+        js/window
+        "https://zebra.joshuadavey.com/api"
+        (clj->js {:method "POST"
+                  :headers {"Content-Type" "application/transit+json"}
+                  :body body}))
+      (.then #(.text %))
+      (.then (partial t/read (t/reader :json))))))
+
 (defn root []
-  [text "pants"])
+  (let [config [(list
+                  {:puzzle/puzzle [:puzzle/clues :puzzle/solution]}
+                  (merge (get difficulties "normal") {:size 4}))]]
+    (fetch-puzzle config))
+  (fn []
+    [view {:style {:align-items "center"
+                   :background-color "#fff"
+                   :flex 1
+                   :justify-content "center"
+                   :padding 20}}
+     [text "pants"]]))
 
 (def app (reactify-component root))
