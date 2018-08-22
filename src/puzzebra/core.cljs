2e500fe312505c8fe95129c9c3254370f31204c0
Brian Dunn <brian@hashrocket.com>
2018-08-22 11:27:40 -0500

    render same-house

--- src/puzzebra/core.cljs
+++ src/puzzebra/core.cljs
@@ -59,32 +59,55 @@
 (defmulti piece :clue/type)
 
 (defmethod piece :left-of [{args :clue/args}]
-  (let [row-offset (min (map first args))
-        cell-style {:width 20
-                    :height 20
+  (let [cell-style {:border-bottom-width 1
                     :border-color "#ccc"
-                    :border-style "solid"
                     :border-right-width 1
-                    :border-bottom-width 1}
+                    :border-style "solid"
+                    :height 20
+                    :width 20}
         cell (fn [i x y] [view {:style (merge cell-style {:background-color (row-color y)})} [text i]])
         empty-cell (fn [x y] [view {:style cell-style}])
-        cells (for [y (let [[first-row last-row] (sort (map first args))] (range first-row (+ 1 last-row)))
+        cells (for [y (let [[first-row last-row] (sort (map first args))]
+                        (range first-row (+ 1 last-row)))
                     x (range 2)]
                 (let [[row value] (nth args x [])]
                   (partial (if (= y row) (partial cell value) empty-cell) x y)))]
-    [view {:style {:border-style "solid"
-                   :border-top-width 1
-                   :border-left-width 1
+    [view {:style {:align-items "flex-start"
                    :border-color "#ccc"
-                   :align-items "flex-start"
-                   :flex-wrap "wrap"
+                   :border-left-width 1
+                   :border-style "solid"
+                   :border-top-width 1
                    :flex-direction "row"
+                   :flex-wrap "wrap"
                    :margin 10
                    :width 42}}
      (doall (map (fn [component i] ^{:key i}[component]) cells (range)))]))
 
+(defmethod piece :same-house [{args :clue/args}]
+  (let [cell-style {:border-bottom-width 1
+                    :border-color "#ccc"
+                    :border-right-width 1
+                    :border-style "solid"
+                    :height 20
+                    :width 20}
+        cell (fn [i y] [view {:style (merge cell-style {:background-color (row-color y)})} [text i]])
+        empty-cell (fn [y] [view {:style cell-style}])
+        cells (for [y (let [[first-row last-row] (sort (map first args))]
+                        (range first-row (+ 1 last-row)))]
+                (let [c (some (fn [[row val]] (and (= row y) val)) args)]
+                  (partial (if c (partial cell c) empty-cell) y)))]
+    [view {:style {:align-items "flex-start"
+                   :border-color "#ccc"
+                   :border-left-width 1
+                   :border-style "solid"
+                   :border-top-width 1
+                   :flex-direction "row"
+                   :flex-wrap "wrap"
+                   :margin 10
+                   :width 21}}
+     (doall (map (fn [component i] ^{:key i}[component]) cells (range)))]))
+
 (defmethod piece :next-to [{paths :clue/args}] [text (.stringify js/JSON (clj->js paths))])
-(defmethod piece :same-house [{paths :clue/args}] [text (.stringify js/JSON (clj->js paths))])
 (defmethod piece :in-house [{paths :clue/args}] [text (.stringify js/JSON (clj->js paths))])
 
 (defn root []
